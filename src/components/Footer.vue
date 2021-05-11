f9bde296e316014519be4800e7d5a932ab041969
Natalia Miarowska <qwertynka15@gmail.com>
2021-05-11 16:51:07 +0200

    Login

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,11 @@
 <template>
-  <v-footer>
+  <v-footer color="#1b2f57">
 
+      <v-row no-gutters>
+        <v-col v-for="ref in refs" justify="right">
+          <router-link class="footer_refs" :to="{path:ref.router}">{{ref.name}}</router-link>
+        </v-col>
+      </v-row>
   </v-footer>
 </template>
 
@@ -9,5 +14,26 @@ import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
 
 @Component
-export default class Footer extends Vue {}
+export default class Footer extends Vue {
+  data() {
+    return {
+      refs: [
+        {router: '/faq', name: 'Pomoc i FAQ'},
+        {router: '/toa', name: 'Polityka prywatności'},
+        {router: '/regulations', name: 'Regulamin'},
+        {router: '/contact', name: 'Kontakt'}
+      ]
+    };
+  }
+}
 </script>
+<style>
+.footer_refs{
+  color:white!important;
+  text-decoration: none;
+  font-weight: 300!important;
+  text-align: right!important;
+  font-size: 15px;
+}
+
+</style>
