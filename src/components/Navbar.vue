530eed94b8d11490cfef8a984367f7ef96d07e02
fhtoma <futomahubert@gmail.com>
2021-05-11 20:43:44 +0200

    Footer+Navbar
    Upiększenie Footera i Navbara. Na smartfonie jest dostępne menu z poszczególnymi podstronami (logowanie, home itp) - trzeba jeszcze poprawić jego wygląd.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,27 +1,24 @@
 <template>
-<<<<<<< Updated upstream
   <div>
     <v-app-bar color="rgb(41,70,129)" dark>
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-    <v-toolbar-title class="d-none d-sm-flex">Apka turniejowa</v-toolbar-title>
-
-    <v-spacer></v-spacer>
-
-    <v-btn icon v-for="item in iconLinks" :key="item.link" :to="item.link">
-      <v-icon>{{ item.icon }}</v-icon>
-=======
-  <div >
-    <v-app-bar color="#1b2f57" class="navbar-content">
-    <v-toolbar-title >Apka turniejowa</v-toolbar-title>
-
-    <v-spacer></v-spacer>
-
-    <v-btn icon v-for="icon in iconLinks" :key="icon.icon" class="navbar-content"> 
-      <v-icon>{{ icon.icon }}</v-icon>
->>>>>>> Stashed changes
-    </v-btn>
-  </v-app-bar>
+      <v-toolbar-title>Apka turniejowa</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon v-for="icon in iconLinks" :key="icon" :to="icon.link">
+        <v-icon>{{ icon.icon }}</v-icon>
+      </v-btn>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" absolute top right temporary>
+        <v-list nav dense>
+          <v-list-item-group v-model="group">
+            <strong>Apka Turniejowa</strong>
+            <v-list-item v-for="icon in iconLinks" :key="icon" :to="icon.link">
+              <v-icon>{{ icon.icon }}</v-icon>
+              <v-list-item-title>{{ icon.text }}</v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
   </div>
 </template>
 
@@ -32,27 +29,34 @@ import { Component } from 'vue-property-decorator';
 export default class Navbar extends Vue {
   data() {
     return {
+      drawer: false,
+      group: null,
       iconLinks: [
         {
-          link:"/",icon: "mdi-seal",
+          link: '/home',
+          icon: 'mdi-seal',
+          text: 'Strona główna',
         },
         {
-          icon: "mdi-account-multiple-outline",
+          link: '',
+          icon: 'mdi-account-multiple-outline',
+          text: 'Kluby',
         },
         {
-          icon: "mdi-bell-outline",
+          link: '',
+          icon: 'mdi-bell-outline',
+          text: 'Powiadomienia',
         },
         {
-          link:"/login",icon: "mdi-login",
+          link: '/login',
+          icon: 'mdi-login',
+          text: 'Logowanie',
         },
       ],
     };
   }
-};
-</script>
-
-<style>
-  .navbar-content{
-    color:white!important;
+  watch(group) {
+    this.drawer = false;
   }
-</style>
+}
+</script>
