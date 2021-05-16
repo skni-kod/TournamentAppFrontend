cbdc8df820465fcac4ae6e04455d46621d6dfa92
Natalia Miarowska <qwertynka15@gmail.com>
2021-05-16 23:16:58 +0200

    Galeria
    - komponent Galeria
    - powiększanie zdjęć
    - przeglądanie galerii

---
+++ src/views/TournamentInfo.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <gallery></gallery>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import Gallery from '../components/Gallery.vue';
+import { Component } from 'vue-property-decorator';
+
+@Component({
+  components: {
+    Gallery
+  },
+})
+
+export default class TournamentInfo extends Vue {
+  data() {
+    return {
+
+    };
+  }
+}
+</script>
