4eb954d43f3e06d5ffc55cb6eec4ef00b903ceef
fhtoma <futomahubert@gmail.com>
2021-06-19 19:16:13 +0200

    Gallery Data Base
    Fajna ta galeria

--- src/components/TournamentGallery.vue
+++ src/components/TournamentGallery.vue
@@ -2,6 +2,11 @@
   <div>
     <v-container>
       <v-row justify="center">
+        <gallery
+          :images="images"
+          :index="index"
+          @close="index = null"
+        ></gallery>
         <v-img
           class="image rounded ma-2"
           v-for="(image, imageIndex) in images"
@@ -21,62 +26,55 @@
 <script lang="ts">
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
+import VueGallery from '@/node_modules/vue-gallery/src/component/gallery.vue';
+import axios from '@/axios';
 
-@Component
+@Component({
+  components: {
+    VueGallery
+  }
+})
 export default class Gallery extends Vue {
-  /*created() {
+  created() {
     this.downloadData();
-  },
+  }
 
   downloadData() {
-    console.log(this.$route.params.id);
     if (this.auth) {
       axios
         .get('tournament/' + this.$route.params.id + '/', {
           headers: {
-            Authorization: 'Bearer ' + this.token,
+            Authorization: 'Bearer ' + this.$store.getters.token,
           },
         })
         .then((res2) => {
           if (res2.status === 200) {
-            console.log('asd');
-            let tabelki = [];
-            const data = res2.data;
-            data.forEach((element) => {
-              let tab = {};
-              console.log(element);
-              tab[0] = element.name;
-              tab[1] = element.date;
-              tab[2] = element.country;
-              tab[3] = element.address;
-              tab[4] = element.members_limit;
-              tab[5] = element.organiser;
-              tab[6] = element.play_type;
-              tabelki.push(tab);
-              console.log(tabelki);
+            let gal:object[] = [];
+            const data = res2.data.gallery.image;
+            data.forEach((element:any) => {
+              let photo = element.image;
+              gal.push(photo);
             });
-            this.$data.info = tabelki;
-            console.log(tabelki);
+            this.$data.images = gal;
           }
         })
         .catch(() => {
-          console.log('Błąd w turniejach');
+          console.log('Błąd w galerii');
         });
     }
-  },
-*/
+  }
+
+  get auth() {
+    return this.$store.getters.isAuthenticated;
+  }
+
   data() {
     return {
       index: null,
-      images: [
-        'https://cdn.pixabay.com/photo/2021/01/29/11/33/chess-5960730_960_720.jpg',
-        'https://cdn.pixabay.com/photo/2017/09/08/02/24/chess-2727443_960_720.jpg',
-        'https://cdn.pixabay.com/photo/2019/03/04/11/59/chess-4033887_960_720.jpg',
-        'https://cdn.pixabay.com/photo/2017/02/24/18/20/king-2095834_960_720.jpg',
-      ],
+      images: [],
     };
   }
-};
+}
 </script>
 
 <style scoped>
