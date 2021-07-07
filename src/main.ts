04952fcde633a94a9ce884ecb77d630cae5f22bc
hekx11 <t.popek123@gmail.com>
2021-07-07 18:43:37 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import vuetify from "./plugins/vuetify";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import vuetify from './plugins/vuetify';
+import * as VueGoogleMaps from 'vue2-google-maps';
 
 Vue.config.productionTip = false;
+Vue.use(VueGoogleMaps, {
+  load: { key: 'AIzaSyBboZx3u3uI7_2UqJk-FiWra60pwJwv3DU' },
+});
 
 new Vue({
   router,
   store,
   vuetify,
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app');
