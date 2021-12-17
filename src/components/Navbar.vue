20532158718626f0a371ceeb142ef4af33976328
Cytrus14 <63610203+Cytrus14@users.noreply.github.com>
2021-12-17 17:54:07 +0100

    changed navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,50 +1,49 @@
 <template>
   <div>
     <v-app-bar dark class="primary">
-      <v-icon v-if="$vuetify.breakpoint.xsOnly">mdi-chess-king</v-icon>
-      <v-toolbar-title v-else>Aplikacja Turniejowa
-      </v-toolbar-title>
+      <v-icon v-if="$vuetify.breakpoint.sm || $vuetify.breakpoint.xs">mdi-chess-king</v-icon>
+      <v-toolbar-title v-else>Aplikacja Turniejowa</v-toolbar-title>
       
-
       <v-spacer></v-spacer>
-      <v-row v-if="$vuetify.breakpoint.xsOnly" justify="end" class="pr-3">
-      <div v-for="(page, i) in pages" :key="i">
-        <v-btn v-if="page.default || auth" icon :to="page.link" class="icon">
-          <v-icon>{{ page.icon }}</v-icon>
-        </v-btn>
-      </div>
+
+      <v-row v-if=" $vuetify.breakpoint.xs" justify="end" class="pr-3">
+        <div v-for="(page, i) in pages" :key="i">
+          <v-btn v-if="page.default || auth" icon :to="page.link" class="icon">
+            <v-icon>{{ page.icon }}</v-icon>
+          </v-btn>
+        </div>
       </v-row>
       
       <v-row justify="end" class="pr-3" v-else>
-      <div v-for="(page, i) in pages" :key="i">
-        <v-btn v-if="page.default || auth" icon :to="page.link" class="icon">
-          <v-icon>{{ page.icon }}</v-icon>
-        </v-btn>
-      </div>
+        <div v-for="(page, i) in pages" :key="i">
+          <v-btn v-if="page.default || auth" icon :to="page.link" class="icon pr-6" width="auto">
+            <v-icon>{{ page.icon }}</v-icon>
+            <h4 justify="start">{{page.text}} </h4>
+          </v-btn>
+        </div>
       </v-row>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
     </v-app-bar>
 
-    <v-navigation-drawer v-model="drawer" absolute top right temporary>
+    <v-navigation-drawer v-model="drawer" absolute top right temporary
+    color="primary"
+    >
       <v-list nav dense shaped>
         <v-list-item-group v-model="group">
-          <h3 class="font-weight-bold pl-2 mb-2">Aplikacja Turniejowa</h3>
-
-          <div v-for="(page, i) in pages" :key="i">
-            <v-list-item v-if="page.default || auth" :to="page.link">
-              <v-icon>{{ page.icon }}</v-icon>
-              <v-list-item-title
-                ><h4 class="grey--text text--darken-2">
-                  {{ page.text }}
-                </h4></v-list-item-title
-              >
-            </v-list-item>
+          <h3 class="font-weight-bold pl-2 mb-2 white--text">Aplikacja Turniejowa</h3>
+            <div v-for="(page, i) in pages" :key="i">
+              <v-list-item v-if="page.default || auth" :to="page.link">
+                <v-icon color="white">{{ page.icon }}</v-icon>
+                <v-list-item-title>
+                  <h4 class="white--text pl-4"> {{ page.text }}</h4>
+                </v-list-item-title>
+              </v-list-item>
           </div>
 
           <v-list-item @click="auth ? logout() : login()">
-            <v-icon>{{ text.icon }}</v-icon>
+            <v-icon color="white">{{ text.icon }}</v-icon>
             <v-list-item-title
-              ><h4 class="grey--text text--darken-2">
+              ><h4 class="white--text pl-4">
                 {{ text.info }}
               </h4></v-list-item-title
             >
@@ -74,7 +73,7 @@ export default class Navbar extends Vue {
           default: true,
         },
         {
-          link: { name: 'User' }, // Insert here a link to the tournaments page
+          link: { name: 'Tutaj_wstaw_link_do_turniejów' }, // Insert here a link to the tournaments page
           icon: 'mdi-trophy-outline',
           text: 'Turnieje',
           default: false,
